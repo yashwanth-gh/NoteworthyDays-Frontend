00d92ad05085d5ba3ee5c6b2cfb478fd0a311a90
Yashwanth B M <yashwanthbm36@gmail.com>
2024-02-03 15:41:25 +0530

    project setup

---
+++ src/conf/conf.ts
@@ -0,0 +1,6 @@
+export const conf = {
+    googleClientId:String(import.meta.env.VITE_GOOGLE_CLIENT_ID),
+    oauthRedirectUri:String(import.meta.env.VITE_OAUTH_REDIRECT_URI),
+    googleOauthUri:String(import.meta.env.VITE_GOOGLE_OAUTH_URI),
+    serverEndpoint:String(import.meta.env.VITE_SERVER_ENDPOINT),
+}
